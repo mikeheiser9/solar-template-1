3e5645e4afcc3678c18620a1c608a72819afc8a2
Kyle Wilson <mike@performancemotif.com>
2018-09-25 17:30:36 -0700

    replace american with state

---
+++ app/public/js/index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+
+	$.getJSON('https://json.geoiplookup.io/', function(data) {
+  		 var currentState = JSON.stringify(data.region, null, 2);
+  		 //console.log("Current state var: " + currentState);
+		 currentState = currentState.replace('"','');
+		 currentState = currentState.replace('"','');
+  		 document.getElementById("title").innerHTML = currentState + "  Homeowners: Going Solar Is Now Easier Than Ever Thanks to These Policies";
+
+});
+
+	
+
+
+
+});
